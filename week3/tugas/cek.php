68684648e4b139daf554b0b73c215dfd9778d26c
abil0321 <salsabilmuhammad45@gmail.com>
2022-10-17 07:36:30 +0800

    nambahin dikit doang

--- week3/tugas/cek.php
+++ week3/tugas/cek.php
@@ -1,70 +1,70 @@
 <?php
-
 # membuat class Animal
-class Animal
-{
+class Animal{
     # property animals
-    public $animals;
+    // public $animals;
+
     # method constructor - mengisi data awal
     # parameter: data hewan (array)
-    public function __construct($animals)
-    {
-        $this->animals = $animals;
+    public function __construct($data){
+        $this->animals = $data;
     }
 
     # method index - menampilkan data animals
-    public function index()
-    {
+    public function index(){
         # gunakan foreach untuk menampilkan data animals (array)
-        foreach ($this->animals as $animal) {
-            echo $animal . "<br>";
+        $animals = $this->animals;
+        foreach ($animals as $animal){
+            echo $animal . '<br>';
         }
     }
 
     # method store - menambahkan hewan baru
     # parameter: hewan baru
-    public function store($data)
-    {
+    public function store($data){
         # gunakan method array_push untuk menambahkan data baru
-        array_push($this->animals, $data);
+        $animals = $this->animals;
+        array_push($animals, $data);
+        // foreach ($animals as $animal){
+        //     echo $animal.'<br>';
+        // }
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
-    public function update($index, $data)
-    {
-        # gunakan index untuk mengupdate data animals
-        $this->animals[$index] = $data;
+    public function update($index, $data){
+
     }
 
     # method delete - menghapus hewan
     # parameter: index
-    public function destroy($index)
-    {
+    public function destroy($index){
         # gunakan method unset atau array_splice untuk menghapus data array
-        unset($this->animals[$index]);
+        $animals = $this->animals;
+        unset($animals[$index]); // Unset
+        foreach ($animals as $animal){
+            echo $animal.'<br>';
+        }
     }
 }
 
 # membuat object
 # kirimkan data hewan (array) ke constructor
-$animal = new Animal(['Kucing', 'Sapi', 'Kambing', 'Bebek']);
+$animal = new Animal(["Ayam", "Ikan"]);
 
 echo "Index - Menampilkan seluruh hewan <br>";
 $animal->index();
 echo "<br>";
 
 echo "Store - Menambahkan hewan baru <br>";
-$animal->store('burung');
+$animal->store('Burung');
 $animal->index();
 echo "<br>";
 
 echo "Update - Mengupdate hewan <br>";
 $animal->update(0, 'Kucing Anggora');
-$animal->index();
 echo "<br>";
 
 echo "Destroy - Menghapus hewan <br>";
 $animal->destroy(1);
-$animal->index();
 echo "<br>";
